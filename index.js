e93a9e575d1a0f56cdb87138e25c919f7a756855
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-03-20 14:45:50 +0530

    installed dotenv+edits

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 app.use(express.static('build'))
 
-const mongoose = require('mongoose')
+
 
 
 
@@ -28,18 +28,6 @@ const mongoose = require('mongoose')
 
 
 // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url = `mongodb+srv://antariksh:<password>@cluster0.ouytb.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 // let notes = [
 //     {
@@ -73,13 +61,7 @@ const Note = mongoose.model('Note', noteSchema)
 //  })
 
 
- noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
- })
+
 
  app.get('/api/notes', (request,response)=> {
     
@@ -91,44 +73,37 @@ const Note = mongoose.model('Note', noteSchema)
  })
 
  app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
- const generateId = () => {
+//  const generateId = () => {
 
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id)) 
-    : 0
+//   const maxId = notes.length > 0
+//     ? Math.max(...notes.map(n => n.id)) 
+//     : 0
 
-  return maxId + 1
+//   return maxId + 1
 
- }
+
+//  }
 
 app.post('/api/notes', (request,response) => {
   const body = request.body
-  
-  if(!body.content){
-    return response.status(400).json({
-      error: "content missing"
-    })
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note ={
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 
