10412735ea812dec6e230d833e4b2008af9b081a
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-03-21 20:33:01 +0530

    lint edits

--- index.js
+++ index.js
@@ -22,112 +22,112 @@ app.use(express.static('build'))
 
 
 
-app.get('/api/notes', (request,response)=> {
-    
-    Note.find({}).then(notes => {
+app.get('/api/notes', (request,response) => {
 
-        response.json(notes)
+  Note.find({}).then(notes => {
 
-    })
+    response.json(notes)
+
+  })
 })
 
 
 
 app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id).then(note => {
-    
-        if (note) {
-            response.json(note)
-        } else {
-            response.status(404).end()
-        }
-    })
+  Note.findById(request.params.id).then(note => {
 
-        .catch(error => next(error))
-  
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+    .catch(error => next(error))
+
 })
 
 app.post('/api/notes', (request,response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (body.content === undefined) {
-        return response.status(400).json({ error: 'content missing' })
-    }
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 
-        .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 
 app.delete('/api/notes/:id', (request, response) => {
-  
-    Note.findByIdAndRemove(request.params.id)
-        // eslint-disable-next-line no-unused-vars
-        .then(result => {
-            response.status(204).end()
-        })
-        // eslint-disable-next-line no-undef
-        .catch(error => next(error))
+
+  Note.findByIdAndRemove(request.params.id)
+  // eslint-disable-next-line no-unused-vars
+    .then(result => {
+      response.status(204).end()
+    })
+  // eslint-disable-next-line no-undef
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const { content, important } = request.body
+  const { content, important } = request.body
 
-    // const note = {
-    //     content: content,
-    //     important: important,
-    // }
+  // const note = {
+  //     content: content,
+  //     important: important,
+  // }
 
-    Note.findByIdAndUpdate(
-        request.params.id, 
-        { content, important },
-        { new: true, runValidators: true, context: 'query' }
-    )
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
 
-    next(error)
+  next(error)
 }
 app.use(errorHandler)
 
 
 
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server running on port  ${PORT}`)
-}) 
+  console.log(`Server running on port  ${PORT}`)
+})
 
 
 
@@ -141,7 +141,7 @@ app.listen(PORT, () => {
 //  const generateId = () => {
 
 //   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id)) 
+//     ? Math.max(...notes.map(n => n.id))
 //     : 0
 
 //   return maxId + 1
@@ -204,7 +204,7 @@ app.listen(PORT, () => {
 //       important: true
 //     }
 //   ]
-/* without express 
+/* without express
   const app = http.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'application/json' })
     response.end(JSON.stringify(notes))
